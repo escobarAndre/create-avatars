bf5d8a976c850b626cc1b957b0879f4af40cc698
André Escobar <andre.escobar@soupilar.com.br>
2022-05-20 13:02:42 -0300

    Run lint

--- src/utils/options/index.js
+++ src/utils/options/index.js
@@ -1,90 +1,90 @@
-import avataaarsOptions from './avataaars';
-import botttsOptions from './bottts';
+import avataaarsOptions from "./avataaars";
+import botttsOptions from "./bottts";
 // import gridyOptions from './gridy';
-import identiconOptions from './identicon';
-import initialsOptions from './initials';
+import identiconOptions from "./identicon";
+import initialsOptions from "./initials";
 // import jdenticonOptions from './jdenticon';
-import micahOptions from './micah';
-import croodlesOptions from './croodles';
-import croodlesNeutralOptions from './croodlesNeutral';
-import pixelArtOptions from './pixelArt';
-import pixelArtNeutralOptions from './pixelArtNeutral';
-import openPeepsOptions from './openPeeps';
-import personasOptions from './personas';
-import miniavsOptions from './miniavs';
-import bigSmileOptions from './bigSmile';
-import bigEarsOptions from './bigEars';
-import bigEarsNeutralOptions from './bigEarsNeutral';
-import adventurerOptions from './adventurer';
-import adventurerNeutralOptions from './adventurerNeutral';
+import micahOptions from "./micah";
+import croodlesOptions from "./croodles";
+import croodlesNeutralOptions from "./croodlesNeutral";
+import pixelArtOptions from "./pixelArt";
+import pixelArtNeutralOptions from "./pixelArtNeutral";
+import openPeepsOptions from "./openPeeps";
+import personasOptions from "./personas";
+import miniavsOptions from "./miniavs";
+import bigSmileOptions from "./bigSmile";
+import bigEarsOptions from "./bigEars";
+import bigEarsNeutralOptions from "./bigEarsNeutral";
+import adventurerOptions from "./adventurer";
+import adventurerNeutralOptions from "./adventurerNeutral";
 
 export default {
-	adventurer: {
-		id: 'adventurer',
-		options: adventurerOptions
-	},
-	adventurer_neutral: {
-		id: 'adventurer_neutral',
-		options: adventurerNeutralOptions
-	},
-	avataaars: {
-		id: 'avataaars',
-		options: avataaarsOptions
-	},
-	big_ears: {
-		id: 'big_ears',
-		options: bigEarsOptions
-	},
-	big_ears_neutral: {
-		id: 'big_ears_neutral',
-		options: bigEarsNeutralOptions
-	},
-	big_smile: {
-		id: 'big_smile',
-		options: bigSmileOptions
-	},
-	bottts: {
-		id: 'bottts',
-		options: botttsOptions
-	},
-	croodles: {
-		id: 'croodles',
-		options: croodlesOptions
-	},
-	croodles_neutral: {
-		id: 'croodles_neutral',
-		options: croodlesNeutralOptions
-	},
-	identicon: {
-		id: 'identicon',
-		options: identiconOptions
-	},
-	initials: {
-		id: 'initials',
-		options: initialsOptions
-	},
-	micah: {
-		id: 'micah',
-		options: micahOptions
-	},
-	miniavs: {
-		id: 'miniavs',
-		options: miniavsOptions
-	},
-	open_peeps: {
-		id: 'open_peeps',
-		options: openPeepsOptions
-	},
-	personas: {
-		id: 'personas',
-		options: personasOptions
-	},
-	pixel_art: {
-		id: 'pixel_art',
-		options: pixelArtOptions
-	},
-	pixel_art_neutral: {
-		id: 'pixel_art_neutral',
-		options: pixelArtNeutralOptions
-	}
+  adventurer: {
+    id: "adventurer",
+    options: adventurerOptions,
+  },
+  adventurer_neutral: {
+    id: "adventurer_neutral",
+    options: adventurerNeutralOptions,
+  },
+  avataaars: {
+    id: "avataaars",
+    options: avataaarsOptions,
+  },
+  big_ears: {
+    id: "big_ears",
+    options: bigEarsOptions,
+  },
+  big_ears_neutral: {
+    id: "big_ears_neutral",
+    options: bigEarsNeutralOptions,
+  },
+  big_smile: {
+    id: "big_smile",
+    options: bigSmileOptions,
+  },
+  bottts: {
+    id: "bottts",
+    options: botttsOptions,
+  },
+  croodles: {
+    id: "croodles",
+    options: croodlesOptions,
+  },
+  croodles_neutral: {
+    id: "croodles_neutral",
+    options: croodlesNeutralOptions,
+  },
+  identicon: {
+    id: "identicon",
+    options: identiconOptions,
+  },
+  initials: {
+    id: "initials",
+    options: initialsOptions,
+  },
+  micah: {
+    id: "micah",
+    options: micahOptions,
+  },
+  miniavs: {
+    id: "miniavs",
+    options: miniavsOptions,
+  },
+  open_peeps: {
+    id: "open_peeps",
+    options: openPeepsOptions,
+  },
+  personas: {
+    id: "personas",
+    options: personasOptions,
+  },
+  pixel_art: {
+    id: "pixel_art",
+    options: pixelArtOptions,
+  },
+  pixel_art_neutral: {
+    id: "pixel_art_neutral",
+    options: pixelArtNeutralOptions,
+  },
 };
